e4d654cd9c7d930568cf42e45a06b0d95d43b695
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2018-12-13 16:24:16 +0100

    added flash
    flashlight and someother stuff

---
+++ Assets/DisableBody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableBody : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Body;
+
+    void Start()
+    {
+        Body.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/FlashLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    public GameObject flashlight;
+    // Use this for initialization
+    void Start()
+    {
+        flashlight.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlashlightToggle();
+    }
+
+    void FlashlightToggle()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashlight.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            flashlight.SetActive(false);
+        }
+    }
+}
