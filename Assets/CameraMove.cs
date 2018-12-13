eb2a3322ec263c11216baf6bea6a4f2fd31d279f
Niklas <niklas.jakobsen@elev.ga.lbs.se>
2018-12-13 12:10:08 +0100

    Added Player
    added player GameObject and Camera

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public float CameraSpeed;
+    [SerializeField]
+    Camera Cam;
+    [SerializeField]
+    GameObject Player;
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Cam.transform.Rotate(Input.GetAxis("Mouse Y") * -CameraSpeed, 0, 0);
+        Player.transform.Rotate(0, Input.GetAxis("Mouse X") * CameraSpeed, 0);
+    }
+}
---
+++ Assets/PlayerMovment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    public float MoveSpeed = 20f;
+    
+    [SerializeField]
+    Rigidbody rbody;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Move;
+        Move = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal") * MoveSpeed,0 , Input.GetAxis("Vertical") * MoveSpeed));
+        Move.y = rbody.velocity.y;
+        rbody.velocity = Move;
+    }
+}
